30ce21a8020a1d19e1051585040e495f4f1bddd8
AlexanderAkh <sancheek240@mail.ru>
2023-01-24 12:26:31 +0300

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите 5-ти значное число:");
+string num = Console.ReadLine();
+int DL = num.Length;
+if (DL == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        System.Console.WriteLine("Да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено не 5-ти значное число");
+}
