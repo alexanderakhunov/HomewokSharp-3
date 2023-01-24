0fcc9d7fdf76071dc43e3508ad6ad1277fe7d54e
AlexanderAkh <sancheek240@mail.ru>
2023-01-24 11:33:16 +0300

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Ввведите Num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        System.Console.WriteLine(Math.Pow(i, 2));
+    }
+}
+else
+{
+    System.Console.WriteLine("Введите другое число");
+}
